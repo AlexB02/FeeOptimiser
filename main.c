5134cfe01d4296c6fae57d8723072e5f25aab0e8
AlexB.02 <alexanderdb8@gmail.com>
2021-05-19 14:59:40 +0100

    Moved build lowest to be a function, array pointer practice

--- main.c
+++ main.c
@@ -2,37 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
-float getRate(char method[]) {
-	if (strcmp(method, "debit") == 0) {
-		return 3.99;
-	}
-	else if (strcmp(method, "standard") == 0) {
-		return 1.49;
-	}
-	return 0;
-}
+float getRate(char method[]);
+void buildLowest(float *lowest, int len, float variableRate);
 
 int main(int argc, char **argv) {
 	int amount = atoi(argv[1]);
 	float lowest[amount];
-	float variableRate = getRate(argv[2]);
-	for (int i=0; i < amount; i++) {
-		if (i <= 9) {
-			lowest[i] = 0.99;
-		}
-		else if (i <= 24) {
-			lowest[i] = 1.49;
-		}
-		else if (i <= 49) {
-			lowest[i] = 1.99;
-		}
-		else if (i <= 199) {
-			lowest[i] = 2.99;
-		} else {
-			printf("Cannot handle more than 200!\n");
-			return 0;
-		}
-	}
-	printf("Rate: %f", lowest[amount - 1]);
+	buildLowest(lowest, amount, getRate(argv[2]));
+	printf("£%d would:"
+	" cost £%0.2f in fees which is %0.2f%%\n",
+	amount, 
+	lowest[amount-1], 
+	(lowest[amount-1] / amount)*100);
 	return 0;
+}
+
+float getRate(char method[]) {
+	if (strcmp(method, "debit") == 0) {
+		return 0.0399;
+	}
+	else if (strcmp(method, "standard") == 0) {
+		return 0.0149;
+	}
+	return 0;
+}
+
+void buildLowest(float *lowest, int len, float variableRate) {
+	for (int i=1; i <= len; i++) { // Set cost in £ for investment of amount i
+		if (i <= 10) {
+			*lowest = 0.99;
+		}
+		else if (i <= 25) {
+			*lowest = 1.49;
+		}
+		else if (i <= 50) {
+			*lowest = 1.99;
+		}
+		else if (i <= 200) {
+			*lowest = 2.99;
+		} else {
+			printf("Cannot handle more than 200!\n"); // rate is unknown for values over 200
+			return;
+		}
+		if (*lowest < i * variableRate) { // Get the highest value to be charged
+			*lowest = i * variableRate;
+		}
+		lowest++;
+	}
 }
